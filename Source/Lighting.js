30f1cf41b04bd56e879b36900fb7d5cf5bc743d2
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2017-12-19 10:34:02 -0500

    Made skeleton- and bone-related classes into transforms collection, and moved transforms to framework.

--- Source/Lighting.js
+++
@@ -1,7 +0,0 @@
-
-function Lighting(ambientIntensity, direction, directionalIntensity)
-{
-	this.ambientIntensity = ambientIntensity;
-	this.direction = direction.clone().normalize();
-	this.directionalIntensity = directionalIntensity;
-}
