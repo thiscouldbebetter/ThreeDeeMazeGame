3f61841796172ce7bc8c93e0ad36c098bf7d7efc
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-11-08 19:03:53 -0500

    Worked on media files, game startup, and configuration.

--- Source/Game.js
+++ Source/Game.js
@@ -1,39 +1,33 @@
 "use strict";
 // main
 class Game {
+    constructor(configuration) {
+        this.configuration = configuration;
+    }
+    static create() {
+        var configuration = Configuration.Instance();
+        return new Game(configuration);
+    }
+    static fromConfiguration(configuration) {
+        return new Game(configuration);
+    }
     start() {
-        var soundHelper = new SoundHelperLive();
-        var mediaLibrary = new MediaLibrary("../Content/", 
-        // images
-        [
-            new Image2("Titles_Opening", "../Content/Images/Titles/Opening.png"),
-            new Image2("Titles_Producer", "../Content/Images/Titles/Producer.png"),
-            new Image2("Titles_Title", "../Content/Images/Titles/Title.png"),
-        ], 
-        // sounds
-        [
-            new SoundFromFile("Sound", "../Content/Audio/Effects/Sound.wav"),
-            new SoundFromFile("Music_Music", "../Content/Audio/Music/Music.mp3"),
-            new SoundFromFile("Music_Producer", "../Content/Audio/Music/Music.mp3"),
-            new SoundFromFile("Music_Title", "../Content/Audio/Music/Title.mp3"),
-        ], 
-        // videos
-        [
-            new Video("Movie", "../Content/Video/Movie.webm"),
-        ], 
-        // fonts
-        [
-            new Font("Font", "../Content/Fonts/Font.ttf")
-        ], 
-        // textStrings
-        []);
+        var contentDirectoryPath = this.configuration.contentDirectoryPath;
+        var manifestFileName = "Manifest.txt";
+        MediaLibrary.mediaFilePathsReadFromContentDirectoryPathAndManifestFileNameThen(contentDirectoryPath, manifestFileName, this.start_MediaFilePathsLoaded);
+    }
+    start_MediaFilePathsLoaded(mediaFilePaths) {
         var colors = Color.Instances();
-        var display = new Display3D(new Coords(320, 240, 2000), new FontNameAndHeight("Font", 10), colors.White, // colorFore,
+        var display = new Display3D(Coords.fromXYZ(320, 240, 2000), new FontNameAndHeight("Font", 10), colors.White, // colorFore,
         colors.Black // colorBack
         );
+        var soundHelper = new SoundHelperLive();
+        var mediaLibrary = MediaLibrary.fromMediaFilePaths(mediaFilePaths);
+        var controlBuilder = ControlBuilder.default();
+        var profileHelper = ProfileHelper.maximal();
         var worldCreator = WorldCreator.fromWorldCreate(WorldExtended.create);
-        var universe = new Universe("ThreeDeeMaze", "0.0.0-20251004-0000", // version
-        new TimerHelper(20), display, soundHelper, mediaLibrary, ControlBuilder.default(), ProfileHelper.maximal(), worldCreator);
+        var universe = new Universe("ThreeDeeMaze", "0.0.0-20251108-0000", // version
+        TimerHelper.fromTicksPerSecond(20), display, soundHelper, mediaLibrary, controlBuilder, profileHelper, worldCreator);
         universe.initialize(() => universe.start());
     }
 }
--- Source/Game.ts
+++ Source/Game.ts
@@ -2,47 +2,55 @@
 
 class Game
 {
+	configuration: Configuration;
+
+	constructor(configuration: Configuration)
+	{
+		this.configuration = configuration;
+	}
+
+	static create(): Game
+	{
+		var configuration = Configuration.Instance();
+		return new Game(configuration);
+	}
+
+	static fromConfiguration(configuration: Configuration): Game
+	{
+		return new Game(configuration);
+	}
+
 	start(): void
 	{
-		var soundHelper = new SoundHelperLive();
-
-		var mediaLibrary = new MediaLibrary
+		var contentDirectoryPath = this.configuration.contentDirectoryPath;
+		var manifestFileName = "Manifest.txt";
+		MediaLibrary.mediaFilePathsReadFromContentDirectoryPathAndManifestFileNameThen
 		(
-			"../Content/",
-			// images
-			[
-				new Image2("Titles_Opening", "../Content/Images/Titles/Opening.png"),
-				new Image2("Titles_Producer", "../Content/Images/Titles/Producer.png"),
-				new Image2("Titles_Title", "../Content/Images/Titles/Title.png"),
-			],
-			// sounds
-			[
-				new SoundFromFile("Sound", "../Content/Audio/Effects/Sound.wav"),
-				new SoundFromFile("Music_Music", "../Content/Audio/Music/Music.mp3"),
-				new SoundFromFile("Music_Producer", "../Content/Audio/Music/Music.mp3"),
-				new SoundFromFile("Music_Title", "../Content/Audio/Music/Title.mp3"),
-			],
-			// videos
-			[
-				new Video("Movie", "../Content/Video/Movie.webm"),
-			],
-			// fonts
-			[
-				new Font("Font", "../Content/Fonts/Font.ttf")
-			],
-			// textStrings
-			[]
+			contentDirectoryPath,
+			manifestFileName,
+			this.start_MediaFilePathsLoaded
 		);
+	}
 
+	start_MediaFilePathsLoaded(mediaFilePaths: string[]): void
+	{
 		var colors = Color.Instances();
 
 		var display = new Display3D
 		(
-			new Coords(320, 240, 2000),
+			Coords.fromXYZ(320, 240, 2000),
 			new FontNameAndHeight("Font", 10),
 			colors.White, // colorFore,
 			colors.Black // colorBack
 		);
+
+		var soundHelper = new SoundHelperLive();
+
+		var mediaLibrary = MediaLibrary.fromMediaFilePaths(mediaFilePaths)
+
+		var controlBuilder = ControlBuilder.default();
+
+		var profileHelper = ProfileHelper.maximal();
 
 		var worldCreator = WorldCreator.fromWorldCreate
 		(
@@ -52,13 +60,13 @@ class Game
 		var universe = new Universe
 		(
 			"ThreeDeeMaze",
-			"0.0.0-20251004-0000", // version
-			new TimerHelper(20),
+			"0.0.0-20251108-0000", // version
+			TimerHelper.fromTicksPerSecond(20),
 			display,
 			soundHelper,
 			mediaLibrary,
-			ControlBuilder.default(),
-			ProfileHelper.maximal(),
+			controlBuilder,
+			profileHelper,
 			worldCreator
 		);
 
